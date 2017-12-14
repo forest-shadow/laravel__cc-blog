c56490b3428de2fc8e541f236c9c671e8b6a1dd9
forest-shadow <mamasay@yandex.ru>
2017-12-14 17:54:38 +0300

    Add author & links

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -2,6 +2,13 @@
 
 @section('main')
     <article class="article">
+        <div class="author">
+            <img class="author__image" src="https://avatars1.githubusercontent.com/u/7695774?s=400&v=4" alt="post author image">
+            <div class="author__details">
+                <a href="#" class="author__name">Somebuddy Someone</a>
+                <div class="author__post-time">5 days go</div>
+            </div>
+        </div>
         <h1 class="article__header">Lorem ipsum dolor sit amet.</h1>
         <h2 class="article__subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis debitis necessitatibus officia.</h2>
         <div class="article__body">
