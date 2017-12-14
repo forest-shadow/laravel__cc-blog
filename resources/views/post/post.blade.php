106d6ae5ed95da97578e964944989425b1127b3f
forest-shadow <mamasay@yandex.ru>
2017-12-14 17:44:45 +0300

    Add the post body
    * downgrade to susy2
    * add typography layout styles
    * add post body markup & styles

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -1,5 +1,15 @@
 @extends('templates.app')
 
 @section('main')
-    <h1>Post/Article Page</h1>
+    <article class="article">
+        <h1 class="article__header">Lorem ipsum dolor sit amet.</h1>
+        <h2 class="article__subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis debitis necessitatibus officia.</h2>
+        <div class="article__body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus ad amet aspernatur at beatae blanditiis delectus deleniti eveniet, maxime necessitatibus nesciunt nobis obcaecati omnis provident quasi, reprehenderit repudiandae tempora!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores commodi doloremque earum error ipsam labore nemo nostrum odio perferendis, praesentium, quidem quis similique, voluptate! Adipisci autem beatae consequatur, deleniti labore numquam possimus quia saepe voluptates! Cumque deleniti eveniet excepturi illo labore minus, pariatur ratione rerum suscipit veniam! Aspernatur assumenda beatae dignissimos dolore eaque est eveniet expedita id ipsum, laboriosam nisi non perferendis praesentium quaerat, quisquam ratione similique soluta, ut velit!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium blanditiis delectus eos esse nam officiis placeat quos unde, veniam?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus ad amet aspernatur at beatae blanditiis delectus deleniti eveniet, maxime necessitatibus nesciunt nobis obcaecati omnis provident quasi, reprehenderit repudiandae tempora!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores commodi doloremque earum error ipsam labore nemo nostrum odio perferendis, praesentium, quidem quis similique, voluptate! Adipisci autem beatae consequatur, deleniti labore numquam possimus quia saepe voluptates! Cumque deleniti eveniet excepturi illo labore minus, pariatur ratione rerum suscipit veniam! Aspernatur assumenda beatae dignissimos dolore eaque est eveniet expedita id ipsum, laboriosam nisi non perferendis praesentium quaerat, quisquam ratione similique soluta, ut velit!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium blanditiis delectus eos esse nam officiis placeat quos unde, veniam?</p>
+        </div>
+    </article>
 @endsection
