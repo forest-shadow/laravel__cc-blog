38c898ec7f106edb97fd1dc4054878cee7163db9
forest-shadow <mamasay@yandex.ru>
2017-12-15 16:42:03 +0300

    Use Markdown for post body
    * add Markdown package
    * update displaying of post's body with Markdown

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -18,7 +18,7 @@
         </div>
         <h1 class="article__header">{{ $post->title }}</h1>
         <h2 class="article__subheader">{{ $post->teaser }}</h2>
-        <div class="article__body">{{ $post->body }}</div>
+        <div class="article__body">{!! Markdown::convertToHtml( e($post->body) ) !!}</div>
 
         @foreach( $post->tags as $tag )
             <a href="{{ route('posts.tagged', $tag->slug ) }}" class="tag">{{ $tag->name }}</a>
